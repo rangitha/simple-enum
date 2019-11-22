9557c890aaf4c1397f85716d642e70f0f4185187
Rangitha Kuruppu <rangitha@live.com>
2019-11-23 03:42:06 +0530

    Allow initializing using a member-value map
    - Add an example for string enums
    - Add an example for heterogeneous enums

--- src/enum.js
+++ src/enum.js
@@ -1,5 +1,13 @@
 module.exports = function (members) {
-  members.forEach(function (member, index) {
-    this[this[index] = member] = index
+  var Enum = this;
+
+  if (members instanceof Array) {
+    return members.forEach(function (member, index) {
+      Enum[Enum[index] = member] = index;
+    });
+  }
+
+  return Object.keys(members).forEach(function (key) {
+    Enum[Enum[members[key]] = key] = members[key];
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const Enum = require('./enum');
-const Status = new Enum(['Failed', 'Success', 'Pending']);
 
-console.log(Status.Failed); // 0
-console.log(Status[Status.Failed]); // 'Failed'
+const Direction = new Enum(['Up', 'Down', 'Left', 'Right']);
+console.log(Direction.Up); // 0
+console.log(Direction[Direction.Up]); // 'Up'
+
+const Response = new Enum({
+  No: 0,
+  Yes: 1
+});
+console.log(Response.No); // 0
+console.log(Response[Response.No]); // 'No'
+
+const Button = new Enum({
+  Confirm: 'OK',
+  Dismiss: 'Cancel'
+});
+console.log(Button.Confirm); // 'OK'
+console.log(Button[Button.Confirm]); // 'Confirm'
+
+const BooleanLikeHeterogeneousEnum = new Enum({
+  No: 0,
+  Yes: 'Yes'
+});
+console.log(BooleanLikeHeterogeneousEnum.No); // 0
+console.log(BooleanLikeHeterogeneousEnum.Yes); // 'Yes'
